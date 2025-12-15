3a7c3690469c3c7d54e6ff4e16fc33fad60fd3fd
gwillis-inovacon <gwillis@inovacon.cl>
2025-12-15 13:55:54 -0300

    Use addCommunitySectionLink to add link to Community section at top

--- javascripts/discourse/api-initializers/affine-sidebar.js
+++ javascripts/discourse/api-initializers/affine-sidebar.js
@@ -8,61 +8,12 @@ export default apiInitializer("1.0.0", (api) => {
     return;
   }
 
-  api.addSidebarSection(
-    (BaseCustomSidebarSection, BaseCustomSidebarSectionLink) => {
-      const AFFiNELink = class extends BaseCustomSidebarSectionLink {
-        get name() {
-          return "affine-workspace";
-        }
-
-        get route() {
-          return null;
-        }
-
-        get href() {
-          return settings.affine_url;
-        }
-
-        get title() {
-          return settings.affine_link_title;
-        }
-
-        get text() {
-          return settings.affine_link_title;
-        }
-
-        get prefixType() {
-          return "icon";
-        }
-
-        get prefixValue() {
-          return settings.affine_link_icon;
-        }
-      };
-
-      const AFFiNESection = class extends BaseCustomSidebarSection {
-        get name() {
-          return "affine";
-        }
-
-        get title() {
-          return settings.affine_link_title;
-        }
-
-        get text() {
-          return "";
-        }
-
-        get links() {
-          return [new AFFiNELink()];
-        }
-
-        get displaySection() {
-          return true;
-        }
-      };
-
-      return AFFiNESection;
-    }
-  );
+  api.addCommunitySectionLink({
+    name: "affine-workspace",
+    route: null,
+    href: settings.affine_url,
+    title: settings.affine_link_title,
+    text: settings.affine_link_title,
+    icon: settings.affine_link_icon,
+  });
 });
